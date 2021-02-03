edc4dcf412087e595c1f31710ad57f4feb6294f1
Ramaleu <ramunas8250@gmail.com>
2021-02-03 11:28:33 +0200

    "ex 6"

--- exercise/exercise6.py
+++ exercise/exercise6.py
@@ -2,6 +2,9 @@ string = input("Enter a word u want to be checked for a palindrome: ")
 string_list = []
 for item in string:
     string_list.append(item)
-
 string_list.reverse()
-print(string_list)
+reverse_joined =''.join(string_list)
+if reverse_joined == string:
+    print("The word is a palindrome")
+else:
+    print("The word is not a palindrome")
